fdec1c4aeee6deda69efef9ebe0cd092640c4acd
Chia-I Lee <znj8bpg9nx@privaterelay.appleid.com>
2026-03-19 15:58:02 +0800

    Update alignment.R

--- scripts/modules/alignment.R
+++ scripts/modules/alignment.R
@@ -1,6 +1,12 @@
-#alignment/prepocessing module
-run_alignment <- function(fastq, out_bam, ref) {
-  cmd <- paste("star --genomeDir", ref, "--readFilesIn", fastq)
+run_alignment <- function(fastq1, fastq2, output_prefix, ref_genome, threads = 8) {
+  
+  cmd <- paste(
+    "STAR",
+    "--runThreadN", threads,
+    "--genomeDir", ref_genome,
+    "--readFilesIn", fastq1, fastq2,
+    "--outFileNamePrefix", output_prefix
+  )
+  
   system(cmd)
 }
-
